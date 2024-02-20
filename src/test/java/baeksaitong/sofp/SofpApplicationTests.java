6f29cd09b84225ed94ca6ae8d2c9337cea01abcf
jansoon <ckdals4600@naver.com>
2024-02-20 15:19:17 +0900

    :tada: Feat: 프로젝트 시작 (#1)

---
+++ src/test/java/baeksaitong/sofp/SofpApplicationTests.java
@@ -0,0 +1,13 @@
+package baeksaitong.sofp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
