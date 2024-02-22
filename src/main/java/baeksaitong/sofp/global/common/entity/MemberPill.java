212d639b017949c6066e0312a9223961ae1793df
jansoon <ckdals4600@naver.com>
2024-02-22 13:23:27 +0900

    :sparkles: Feat: MemberPill 엔티티 추가 (#3)

---
+++ src/main/java/baeksaitong/sofp/global/common/entity/MemberPill.java
@@ -0,0 +1,25 @@
+package baeksaitong.sofp.global.common.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class MemberPill {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "pill_id")
+    private Pill pill;
+}
