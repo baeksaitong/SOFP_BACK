e42e4f63132942ca06ba8ea1c77833471f25e01a
jansoon <ckdals4600@naver.com>
2024-02-22 12:36:09 +0900

    :sparkles: Feat: 이메일,전화번호 정규식 추가 (#3)

---
+++ src/main/java/baeksaitong/sofp/global/common/Constants.java
@@ -0,0 +1,6 @@
+package baeksaitong.sofp.global.common;
+
+public class Constants {
+    private static final String EMAil_REGEXP = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$";
+    private static final String PHONE_NUM_REGEXP = "^01(?:0|1|[6-9])(?:\\d{3}|\\d{4})\\d{4}$";
+}
