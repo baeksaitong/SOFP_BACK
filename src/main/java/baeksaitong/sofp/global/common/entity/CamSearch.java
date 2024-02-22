4edbe25080a35f1d31a541f48dacf37ff4f7a50c
jansoon <ckdals4600@naver.com>
2024-02-22 13:03:01 +0900

    :sparkles: Feat: CamSearch 엔티티 추가 (#3)

---
+++ src/main/java/baeksaitong/sofp/global/common/entity/CamSearch.java
@@ -0,0 +1,24 @@
+package baeksaitong.sofp.global.common.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class CamSearch {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "img_url")
+    private String imgUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "history_id")
+    private Member member;
+}
