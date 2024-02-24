c035a1b1cf483dcbd68076931d78723b679b42d8
jansoon <ckdals4600@naver.com>
2024-02-24 09:26:33 +0900

    :sparkles: Feat: 메일 유효성 검증 추가 (#5)
    :sparkles: Feat: 메일 유효성 검증 추가 (#5)

--- src/main/java/baeksaitong/sofp/domain/member/controller/MemberController.java
+++ src/main/java/baeksaitong/sofp/domain/member/controller/MemberController.java
@@ -1,7 +1,11 @@
 package baeksaitong.sofp.domain.member.controller;
 
+import baeksaitong.sofp.domain.member.dto.request.CheckEmailCodeReq;
 import baeksaitong.sofp.domain.member.service.MemberService;
+import baeksaitong.sofp.global.common.Constants;
 import baeksaitong.sofp.global.common.dto.BaseResponse;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +18,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/mail/sendCode")
-    public ResponseEntity<String> sendEmailCode(@RequestParam String email){
+    public ResponseEntity<String> sendEmailCode(@RequestParam @Valid @Pattern(regexp = Constants.EMAil_REGEXP, message = "이메일 형식이 일치하지 않습니다.") String email){
         memberService.sendEmailCode(email);
         return BaseResponse.ok("인증 코드를 발송하였습니다.");
     }
