126da5035ec5cc2bf3aeeb4aaa5971a2f6c91fae
jansoon <ckdals4600@naver.com>
2024-02-24 13:04:47 +0900

    :sparkles: Feat: 이메일 인증 Request Dto 생성 (#5)

--- src/main/java/baeksaitong/sofp/domain/auth/controller/AuthController.java
+++ src/main/java/baeksaitong/sofp/domain/auth/controller/AuthController.java
@@ -1,13 +1,12 @@
 package baeksaitong.sofp.domain.auth.controller;
 
 import baeksaitong.sofp.domain.auth.dto.request.CheckEmailCodeReq;
+import baeksaitong.sofp.domain.auth.dto.request.SendEmailCodeReq;
 import baeksaitong.sofp.domain.auth.service.AuthService;
-import baeksaitong.sofp.global.common.Constants;
 import baeksaitong.sofp.global.common.dto.BaseResponse;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,14 +16,14 @@ public class AuthController {
 
     private final AuthService authService;
 
-    @GetMapping("/mail/send")
-    public ResponseEntity<String> sendEmailCode(@RequestParam @Valid @Pattern(regexp = Constants.EMAil_REGEXP, message = "이메일 형식이 일치하지 않습니다.") String email){
-        authService.sendEmailCode(email);
+    @PostMapping("/mail/send")
+    public ResponseEntity<String> sendEmailCode(@RequestBody @Validated SendEmailCodeReq req){
+        authService.sendEmailCode(req.getEmail());
         return BaseResponse.ok("인증 코드를 발송하였습니다.");
     }
 
     @PostMapping("/mail/check")
-    public ResponseEntity<Boolean> checkEmailCode(@RequestBody @Valid CheckEmailCodeReq req){
+    public ResponseEntity<Boolean> checkEmailCode(@RequestBody @Validated CheckEmailCodeReq req){
         boolean verified = authService.checkEmailCode(req.getEmail(), req.getCode());
         return BaseResponse.ok(verified);
     }
