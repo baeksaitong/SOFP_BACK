ca7ea92548c9b30ad80af5aa0136bed0e2c11b19
jansoon <ckdals4600@naver.com>
2024-02-24 09:15:06 +0900

    :sparkles: Feat: 회원가입 시 이메일 인증 코드 전송 기능 추가 (#5)

--- src/main/java/baeksaitong/sofp/domain/member/controller/MemberController.java
+++ src/main/java/baeksaitong/sofp/domain/member/controller/MemberController.java
@@ -1,11 +1,21 @@
 package baeksaitong.sofp.domain.member.controller;
 
+import baeksaitong.sofp.domain.member.service.MemberService;
+import baeksaitong.sofp.global.common.dto.BaseResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/app/member")
 @RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/mail/sendCode")
+    public ResponseEntity<String> sendEmailCode(@RequestParam String email){
+        memberService.sendEmailCode(email);
+        return BaseResponse.ok("인증 코드를 발송하였습니다.");
+    }
 }
--- src/main/java/baeksaitong/sofp/domain/member/service/MemberService.java
+++ src/main/java/baeksaitong/sofp/domain/member/service/MemberService.java
@@ -1,9 +1,44 @@
 package baeksaitong.sofp.domain.member.service;
 
+import baeksaitong.sofp.global.common.service.RedisService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.Random;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
+
+    private final MailService mailService;
+    private final RedisService redisService;
+
+    private final int CODE_LENGTH = 6;
+
+    @Value("${auth-code-expiration-minutes}")
+    private final long CODE_MINUTE;
+
+    public void sendEmailCode(String email) {
+        String code = makeRandomNumber();
+        String title = "회원가입 인증 이메일 입니다.";
+        String content =
+                "인증번호 : " +  code +
+                        "<br> 유효시간은 " + CODE_MINUTE + "분 입니다." +
+                        "<br> 인증번호를 제대로 입력해주세요";
+
+        mailService.mailSend(email,title,content);
+        redisService.setValues(email, code, Duration.ofMinutes(CODE_MINUTE));
+    }
+
+    private String makeRandomNumber() {
+        Random r = new Random();
+        StringBuilder randomNumber = new StringBuilder();
+        for(int i = 0; i < CODE_LENGTH; i++) {
+            randomNumber.append(r.nextInt(10));
+        }
+
+        return randomNumber.toString();
+    }
 }
