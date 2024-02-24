bbc98dfe56b2ddcf8fb45e62b18086b46fb8e0e1
jansoon <ckdals4600@naver.com>
2024-02-24 17:18:18 +0900

    :sparkles: Feat: 회원가입 API 추가 (#5)

--- src/main/java/baeksaitong/sofp/domain/auth/controller/AuthController.java
+++ src/main/java/baeksaitong/sofp/domain/auth/controller/AuthController.java
@@ -1,7 +1,10 @@
 package baeksaitong.sofp.domain.auth.controller;
 
+import baeksaitong.sofp.domain.auth.dto.request.SignUpReq;
 import baeksaitong.sofp.domain.auth.service.AuthService;
+import baeksaitong.sofp.global.common.dto.BaseResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,4 +13,11 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final AuthService authService;
+
+    @PostMapping("/sign-up")
+    public ResponseEntity<String> signUp(@RequestBody SignUpReq req){
+        authService.singUp(req);
+        return BaseResponse.ok("회원 가입에 성공했습니다.");
+    }
+
 }
--- src/main/java/baeksaitong/sofp/domain/auth/service/AuthService.java
+++ src/main/java/baeksaitong/sofp/domain/auth/service/AuthService.java
@@ -1,9 +1,33 @@
 package baeksaitong.sofp.domain.auth.service;
 
+import baeksaitong.sofp.domain.auth.dto.request.SignUpReq;
+import baeksaitong.sofp.domain.member.repository.MemberRepository;
+import baeksaitong.sofp.global.common.entity.Member;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import static baeksaitong.sofp.global.common.entity.enums.MemberRole.ROLE_USER;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
+
+    private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+
+    public void singUp(SignUpReq req) {
+        Member member = Member.builder()
+                .name(req.getName())
+                .uid(req.getEmail())
+                .birthday(req.getBirthday())
+                .pwd(passwordEncoder.encode(req.getPassword()))
+                .phoneNumber(req.getPhone())
+                .advertisement(req.getAdvertisement())
+                .role(ROLE_USER)
+                .build();
+
+        memberRepository.save(member);
+    }
 }
