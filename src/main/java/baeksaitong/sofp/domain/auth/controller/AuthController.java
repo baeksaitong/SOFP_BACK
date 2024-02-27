ff9780de61e762f8230d597fdceebc6a96ae0839
jansoon <ckdals4600@naver.com>
2024-02-27 09:06:37 +0900

    :sparkles: Feat: 로그인 API Swagger 문구 추가 (#5)

--- src/main/java/baeksaitong/sofp/domain/auth/controller/AuthController.java
+++ src/main/java/baeksaitong/sofp/domain/auth/controller/AuthController.java
@@ -36,9 +36,9 @@ public class AuthController {
     }
 
     @Operation(tags = "1. Auth", summary = "아이디 중복 검사", description = "아이디 중복 여부를 검사합니다.")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "사용가능 한 아이디입니다."),
-            @ApiResponse(responseCode = "404", description = "code: A-000 | message: 이미 존재하는 아이디입니다.",
+            @ApiResponses({
+                    @ApiResponse(responseCode = "200", description = "사용가능 한 아이디입니다."),
+                    @ApiResponse(responseCode = "404", description = "code: A-000 | message: 이미 존재하는 아이디입니다.",
                     content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
     @PostMapping("/id-check")
@@ -47,6 +47,14 @@ public class AuthController {
         return BaseResponse.ok("사용가능 한 아이디입니다.");
     }
 
+    @Operation(tags = "1. Auth", summary = "로그인", description = "로그인을 진행합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "access 토큰"),
+            @ApiResponse(responseCode = "404", description = "code: A-001 | message: 존재하지 않는 아이디입니다.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "404", description = "code: A-002 | message: 비밀번호가 일치하지 않습니다.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
     @PostMapping("/login")
     public ResponseEntity<LoginRes> login(@RequestBody LoginReq req){
         return BaseResponse.ok(new LoginRes(authService.login(req)));
