8726882b5af295bfac521e45b1d4bfa6a84fff1b
jansoon <ckdals4600@naver.com>
2024-02-27 18:42:41 +0900

    :sparkles: Feat: Naver feign 추가 (#8)

---
+++ src/main/java/baeksaitong/sofp/domain/auth/feign/NaverFeignClient.java
@@ -0,0 +1,27 @@
+package baeksaitong.sofp.domain.auth.feign;
+
+import baeksaitong.sofp.domain.auth.dto.naver.NaverProfile;
+import baeksaitong.sofp.domain.auth.dto.naver.NaverToken;
+import baeksaitong.sofp.global.config.FeignConfig;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.net.URI;
+
+@FeignClient(name = "naverClient", configuration = FeignConfig.class)
+public interface NaverFeignClient {
+    @PostMapping
+    NaverToken getToken(URI baseUrl,
+                        @RequestParam("grant_type") String grantType,
+                        @RequestParam("client_id") String clientId,
+                        @RequestParam("client_secret") String clientSecret,
+                        @RequestParam("code") String code,
+                        @RequestParam("state") String state
+    );
+
+    @GetMapping
+    NaverProfile getProfile(URI baseUrl, @RequestHeader("Authorization") String token);
+}
