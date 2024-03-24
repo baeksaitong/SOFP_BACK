88441e5f5196c13850ecb43f0014de82404d08b5
Jansoon <ckdals4600@naver.com>
2024-03-24 12:46:43 +0900

    :sparkles: Feat: 검색 기록 및 최근 본 알약 Entity -> Collection 변경 (#16)

---
+++ src/main/java/baeksaitong/sofp/global/common/collection/History.java
@@ -0,0 +1,20 @@
+package baeksaitong.sofp.global.common.collection;
+
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@Builder
+@Document(collation = "history")
+public class History {
+    @Id
+    private String id;
+    private List<String> commonHistory;
+    private List<String> imageHistory;
+    private List<String> recentViewPill;
+
+}
