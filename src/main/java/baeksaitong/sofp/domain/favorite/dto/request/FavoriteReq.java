1119ed5b99d79867b7920bc38bd738443680a363
Jansoon <ckdals4600@naver.com>
2024-03-30 00:22:52 +0900

    :memo: Docs: Favorite DTO Swagger 설정 (#25)

--- src/main/java/baeksaitong/sofp/domain/favorite/dto/request/FavoriteReq.java
+++ src/main/java/baeksaitong/sofp/domain/favorite/dto/request/FavoriteReq.java
@@ -1,14 +1,25 @@
 package baeksaitong.sofp.domain.favorite.dto.request;
 
 import baeksaitong.sofp.domain.favorite.dto.enums.SearchType;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
+@AllArgsConstructor
 @NoArgsConstructor
 public class FavoriteReq {
+    @Schema(description = "검색 타입", example = "COMMON or IMAGE")
+    @NotNull(message = "검색 타입이 필요합니다.")
     private SearchType searchType;
-    private Long pillId;
+
+    @Schema(description = "알약 고유 번호", example = "200808876")
+    @NotNull(message = "알약 고유 번호가 필요합니다.")
+    private Long pillSeralNumber;
+
+    @Schema(description = "이미지 검색 시 사용한 이미지")
     private MultipartFile image;
 }
