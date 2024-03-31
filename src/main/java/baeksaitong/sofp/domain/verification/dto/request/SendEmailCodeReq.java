7921b29ae7138516a660ad1a5521fa0b84027fa6
Jansoon <ckdals4600@naver.com>
2024-04-01 00:47:37 +0900

    :memo: Docs: Verification DTO Swagger 설정 (#25)

--- src/main/java/baeksaitong/sofp/domain/verification/dto/request/SendEmailCodeReq.java
+++ src/main/java/baeksaitong/sofp/domain/verification/dto/request/SendEmailCodeReq.java
@@ -1,7 +1,9 @@
 package baeksaitong.sofp.domain.verification.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,8 +11,10 @@ import static baeksaitong.sofp.global.common.Constants.EMAIL_REGEXP;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class SendEmailCodeReq {
     @NotBlank
     @Pattern(regexp = EMAIL_REGEXP, message = "이메일 형식이 일치하지 않습니다.")
+    @Schema(description = "코드 전송할 이메일", example = "example@example.com")
     private String email;
 }
