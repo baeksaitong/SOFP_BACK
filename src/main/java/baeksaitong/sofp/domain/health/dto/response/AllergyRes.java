38c34e53900b8b6948bf73d70534a3dfebe9a976
Jansoon <ckdals4600@naver.com>
2024-04-01 00:05:23 +0900

    :memo: Docs: Health DTO Swagger 설정 (#25)

--- src/main/java/baeksaitong/sofp/domain/health/dto/response/AllergyRes.java
+++ src/main/java/baeksaitong/sofp/domain/health/dto/response/AllergyRes.java
@@ -1,7 +1,12 @@
 package baeksaitong.sofp.domain.health.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.List;
 
-public record AllergyRes(List<String> AllergyList){
+public record AllergyRes(
+        @Schema(description = "알레르기 전체 리스트")
+        List<String> allergyList
+){
 
 }
--- src/main/java/baeksaitong/sofp/domain/health/dto/response/DiseaseRes.java
+++ src/main/java/baeksaitong/sofp/domain/health/dto/response/DiseaseRes.java
@@ -1,6 +1,11 @@
 package baeksaitong.sofp.domain.health.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.List;
 
-public record DiseaseRes(List<String> DiseaseRes) {
+public record DiseaseRes(
+        @Schema(description = "질병 전체 리스트")
+        List<String> diseaseRes
+) {
 }
