d059964d5803768a5ec77d46322d6acb411f789c
Jansoon <ckdals4600@naver.com>
2024-05-07 20:58:12 +0900

    :sparkles: Feat: Redis Flag enum 추가 (#39)

---
+++ src/main/java/baeksaitong/sofp/global/redis/RedisPrefix.java
@@ -0,0 +1,15 @@
+package baeksaitong.sofp.global.redis;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.Duration;
+
+@Getter
+@AllArgsConstructor
+public enum RedisPrefix {
+    REFRESH_TOKEN(Duration.ofDays(180)),
+    SING_UP(Duration.ofMinutes(10));
+
+    final Duration duration;
+}
