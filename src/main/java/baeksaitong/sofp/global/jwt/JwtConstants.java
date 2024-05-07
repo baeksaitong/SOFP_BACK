9ec92cdcfa57d19cfabc64a021681bb8ead67c52
Jansoon <ckdals4600@naver.com>
2024-05-08 01:26:18 +0900

    :sparkles: Feat: 토큰에 token_type 클레임 정보 추가  (#39)

---
+++ src/main/java/baeksaitong/sofp/global/jwt/JwtConstants.java
@@ -0,0 +1,16 @@
+package baeksaitong.sofp.global.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum JwtConstants {
+    AUTHORITIES_KEY("Authorization"),
+    TOKEN_TYPE("token_type"),
+    ACCESS_TOKEN_TYPE("access"),
+    REFRESH_TOKEN_TYPE("refresh");
+
+    final String value;
+
+}
--- src/main/java/baeksaitong/sofp/global/jwt/error/code/JwtErrorCode.java
+++ src/main/java/baeksaitong/sofp/global/jwt/error/code/JwtErrorCode.java
@@ -9,10 +9,12 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum JwtErrorCode implements ErrorCode {
 
-    INVALID_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "J-000", "유효하지 않은 jwt 토큰입니다."),
-    EXPIRED_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "J-001", "만료된 jwt 토큰입니다."),
-    UNSUPPORTED_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "J-002", "지원하지 않는 JWT 토큰입니다."),
-    EMPTY_TOKEN(HttpStatus.BAD_REQUEST, "J-003", "토큰이 필요합니다.");
+    INVALID_JWT_TOKEN(HttpStatus.BAD_REQUEST, "J-000", "유효하지 않은 jwt 토큰입니다."),
+    EXPIRED_JWT_TOKEN(HttpStatus.BAD_REQUEST, "J-001", "만료된 jwt 토큰입니다."),
+    UNSUPPORTED_JWT_TOKEN(HttpStatus.BAD_REQUEST, "J-002", "지원하지 않는 JWT 토큰입니다."),
+    EMPTY_TOKEN(HttpStatus.BAD_REQUEST, "J-003", "토큰이 필요합니다."),
+    CANNOT_REFRESH(HttpStatus.BAD_REQUEST, "J-004", "토큰을 갱신할 수 없습니다."),
+    INVALID_REFRESH(HttpStatus.BAD_REQUEST, "J-004", "refresh 토큰이 아닙니다.");
 
     private final HttpStatus status;
     private final String code;
