d241794e6998b1b55e7b7f245d9767d10e49bb75
Jansoon <ckdals4600@naver.com>
2024-05-13 08:41:14 +0900

    :memo: Docs: 비밀번호 및 성별 유효성 추가 (#44)

--- src/main/java/baeksaitong/sofp/domain/auth/dto/request/SignUpReq.java
+++ src/main/java/baeksaitong/sofp/domain/auth/dto/request/SignUpReq.java
@@ -1,6 +1,7 @@
 package baeksaitong.sofp.domain.auth.dto.request;
 
 import baeksaitong.sofp.global.common.entity.enums.MemberGender;
+import baeksaitong.sofp.global.common.validation.ValidEnum;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -19,24 +20,18 @@ public class SignUpReq {
     @NotBlank(message = "이름은 공백일 수 없습니다.")
     @Schema(description = "이름")
     private String name;
-
     @NotNull(message = "생년월일이 필요합니다.")
     @Schema(description = "생년월일")
     private LocalDate birthday;
-
-    @NotBlank(message = "아이디는 공백일 수 없습니다.")
     @Pattern(regexp = EMAIL_REGEXP, message = "이메일 형식이 일치하지 않습니다.")
     @Schema(description = "아이디(이메일)", example = "example@example.com")
     private String email;
-
-    @NotNull(message = "성별 정보가 필요합니다.")
-    @Schema(description = "성별")
+    @ValidEnum(enumClass = MemberGender.class, message = "유효하지 않은 성별입니다", ignoreCase=true)
+    @Schema(description = "성별", example = "MALE(male)/FEMALE(female)")
     private MemberGender gender;
-
-    @NotBlank(message = "비밀번호는 공백일 수 없습니다.")
-    @Schema(description = "비밀번호")
+    @Pattern(regexp = PW_REGEXP, message = "유효하지 않는 비밀번호입니다.")
+    @Schema(description = "비밀번호 : 8자 이상 16자 이하, 하나 이상 숫자 및 알파벳 문자(소문자 또는 대문자)")
     private String password;
-
     @NotNull(message = "광고 동의 여부가 필요합니다.")
     @Schema(description = "광고 동의 여부")
     private Boolean advertisement;
