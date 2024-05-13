9fc2ff66b365bd7b96f33eab174ddf3fd6a53146
Jansoon <ckdals4600@naver.com>
2024-05-13 09:53:14 +0900

    :memo: Docs: 예시 추가 (#44)

--- src/main/java/baeksaitong/sofp/domain/search/dto/request/KeywordReq.java
+++ src/main/java/baeksaitong/sofp/domain/search/dto/request/KeywordReq.java
@@ -7,17 +7,17 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class KeywordReq {
-    @Schema(description = "검색 키워드 - 현제는 알약 이름만 가능")
+    @Schema(description = "검색 키워드 - 알약 이름, 성분, 효능 정보",example = "가스디알정")
     private String keyword;
-    @Schema(description = "알약 모양")
+    @Schema(description = "알약 모양", example = "원형")
     private String shape;
-    @Schema(description = "알약에 적힌 기호")
+    @Schema(description = "알약에 적힌 기호", example = "IDG")
     private String sign;
-    @Schema(description = "알약 색깔")
+    @Schema(description = "알약 색깔", example = "연두")
     private String color;
-    @Schema(description = "알약 제형")
+    @Schema(description = "알약 제형", example = "경질/연질/정제")
     private String formulation;
-    @Schema(description = "알약 분할선")
+    @Schema(description = "알약 분할선", example = "-/+")
     private String line;
     @Schema(description = "검색 페이지 - 0부터 시작", defaultValue = "0")
     private int page=0;
