435587bfbe4f090f4a02b4d92ec330765b868ce2
jansoon <ckdals4600@naver.com>
2024-05-14 11:40:46 +0900

    :recycle: Refactor: 이미지 리스트 형식으로 변경 (#45)

--- src/main/java/baeksaitong/sofp/domain/search/dto/request/ImageReq.java
+++ src/main/java/baeksaitong/sofp/domain/search/dto/request/ImageReq.java
@@ -5,18 +5,20 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 @Getter
 public class ImageReq {
     @Schema(description = "검색 이미지")
     @NotNull(message = "이미지가 필요합니다.")
-    private MultipartFile image;
+    private List<MultipartFile> images;
     @Schema(description = "검색 페이지 - 0부터 시작, 기본값 : 0")
     private Integer page;
     @Schema(description = "페이지 당 요소 갯수 - 기본값 : 5")
     private Integer limit;
 
-    public ImageReq(MultipartFile image, Integer page, Integer limit) {
-        this.image = image;
+    public ImageReq(List<MultipartFile> images, Integer page, Integer limit) {
+        this.images = images;
         this.page = (page != null) ? page : 0;
         this.limit = (limit != null) ? limit : 5;
     }
