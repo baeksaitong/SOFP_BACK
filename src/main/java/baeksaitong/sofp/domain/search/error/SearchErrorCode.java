3d08ac10edca80de3410e5ae62c06ff8f1c81e63
Jansoon <ckdals4600@naver.com>
2024-05-21 21:08:56 +0900

    :memo: Docs: 검색 API 스웨거 및 유효성 검사 작업 완료 (#52)

--- src/main/java/baeksaitong/sofp/domain/search/error/SearchErrorCode.java
+++ src/main/java/baeksaitong/sofp/domain/search/error/SearchErrorCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum SearchErrorCode implements ErrorCode {
     PILL_INFO_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"S-000","알약 정보를 불려오는데 실패했습니다."),
-    NO_SUCH_PILL(HttpStatus.BAD_REQUEST,"S-001","알약 정보가 존재하지 않습니다")
+    NO_SUCH_PILL(HttpStatus.INTERNAL_SERVER_ERROR,"S-001","알약 정보가 존재하지 않습니다")
     ;
     private final HttpStatus status;
     private final String code;
