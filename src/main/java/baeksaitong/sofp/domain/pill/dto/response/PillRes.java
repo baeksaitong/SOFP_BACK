6ebec4d26c4909eb86d28afc61303f48b937caaa
Jansoon <ckdals4600@naver.com>
2024-05-21 21:08:24 +0900

    :memo: Docs: 복용중인 알약 API 스웨거 및 유효성 검사 작업 완료 (#52)

--- src/main/java/baeksaitong/sofp/domain/pill/dto/response/PillRes.java
+++ src/main/java/baeksaitong/sofp/domain/pill/dto/response/PillRes.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 
 public record PillRes(
-        @Schema(description = "사용자가 현제 복용중인 알약 정보 리스트")
+        @Schema(description = "사용자가 현재 복용중인 알약 정보 리스트")
         List<PillInfoDTO> pillInfoList
 ) {
 
