054ae6bafb9afac98dc58c97553a617db19f279f
Jansoon <ckdals4600@naver.com>
2024-05-21 21:06:52 +0900

    :memo: Docs: 즐겨찾기 API 스웨거 및 유효성 검사 작업 완료 (#52)

--- src/main/java/baeksaitong/sofp/domain/favorite/controller/FavoriteController.java
+++ src/main/java/baeksaitong/sofp/domain/favorite/controller/FavoriteController.java
@@ -4,7 +4,10 @@ import baeksaitong.sofp.domain.favorite.dto.request.FavoriteReq;
 import baeksaitong.sofp.domain.favorite.dto.response.FavoriteRes;
 import baeksaitong.sofp.domain.favorite.service.FavoriteService;
 import baeksaitong.sofp.global.common.dto.BaseResponse;
+import baeksaitong.sofp.global.error.dto.ErrorResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -21,32 +24,50 @@ public class FavoriteController {
 
     private final FavoriteService favoriteService;
 
-    @Operation(summary = "\uD83D\uDD11 즐겨찾기 추가", description = "알약을 즐겨찾기에 추가합니다.")
+    @Operation(summary = "\uD83D\uDD11 즐겨찾기 추가", description = "알약 시리얼 번호로 즐겨찾기를 추가합니다.")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "즐겨찾기 추가 성공")
+            @ApiResponse(responseCode = "200", description = "즐겨찾기 추가 성공"),
+            @ApiResponse(responseCode = "404", description = "code: P-000 | message: 존재하지 않는 알약입니다. <br>" +
+                    "code: U-001 | message: 프로필이 존재하지 않습니다. <br>" +
+                    "code: F-000 | message: 이미 즐겨찾기에 추가되어 있습니다.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
     @PostMapping("/add")
-    private ResponseEntity<String> addFavorite(@RequestParam Long profileId, @ModelAttribute @Validated FavoriteReq req){
+    private ResponseEntity<String> addFavorite(
+            @RequestParam @Schema(name = "프로필 ID") Long profileId,
+            @ModelAttribute @Validated FavoriteReq req
+    ){
         favoriteService.addFavorite(req, profileId);
         return BaseResponse.ok("즐겨찾기 추가 성공");
     }
 
-    @Operation(summary = "\uD83D\uDD11 즐겨찾기 삭제", description = "알약을 즐겨찾기에서 삭제합니다.")
+    @Operation(summary = "\uD83D\uDD11 즐겨찾기 삭제", description = "알약 시리얼 번호로 즐겨찾기에서 삭제합니다.")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "즐겨찾기 삭제 성공")
+            @ApiResponse(responseCode = "200", description = "즐겨찾기 삭제 성공"),
+            @ApiResponse(responseCode = "404", description = "code: P-000 | message: 존재하지 않는 알약입니다. <br>" +
+                    "code: U-001 | message: 프로필이 존재하지 않습니다. <br>" +
+                    "code: F-001 | message: 해당 즐겨찾기 정보가 존재하지 않습니다.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
     @GetMapping("/delete")
-    private ResponseEntity<String> deleteFavorite(@RequestParam Long pillSerialNumber, @RequestParam Long profileId){
+    private ResponseEntity<String> deleteFavorite(
+            @RequestParam @Schema(name = "알약 시리얼 번호") Long pillSerialNumber,
+            @RequestParam @Schema(name = "프로필 ID") Long profileId
+    ){
         favoriteService.deleteFavorite(pillSerialNumber, profileId);
         return BaseResponse.ok("즐겨찾기 삭제 성공");
     }
 
-    @Operation(summary = "\uD83D\uDD11 즐겨찾기 조회", description = "즐겨찾기 된 알약을 조회하니다.")
+    @Operation(summary = "\uD83D\uDD11 즐겨찾기 조회", description = "즐겨찾기에 추가된 알약 리스트를 조회합니다.")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "즐겨찾기에 등록된 알약 리스트 획득")
+            @ApiResponse(responseCode = "200", description = "즐겨찾기에 등록된 알약 리스트 획득"),
+            @ApiResponse(responseCode = "404", description = "code: U-001 | message: 프로필이 존재하지 않습니다.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
     @GetMapping
-    private ResponseEntity<FavoriteRes> getFavorite(@RequestParam Long profileId){
+    private ResponseEntity<FavoriteRes> getFavorite(
+            @RequestParam @Schema(name = "프로필 ID") Long profileId
+    ){
         FavoriteRes res = favoriteService.getFavorite(profileId);
         return BaseResponse.ok(res);
     }
--- src/main/java/baeksaitong/sofp/domain/favorite/dto/response/FavoriteRes.java
+++ src/main/java/baeksaitong/sofp/domain/favorite/dto/response/FavoriteRes.java
@@ -1,8 +1,11 @@
 package baeksaitong.sofp.domain.favorite.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.List;
 
 public record FavoriteRes(
+        @Schema(name = "즐겨찾기 알약 리스트")
         List<FavoriteDto> favoriteList
 ) {
 }
