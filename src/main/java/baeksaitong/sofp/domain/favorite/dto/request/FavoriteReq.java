1f25de1378baf99d0f0677a9c98acf95f7bdb789
Jansoon <ckdals4600@naver.com>
2024-05-27 17:19:13 +0900

    :memo: Rename: 오타 수정 (#78)

--- src/main/java/baeksaitong/sofp/domain/favorite/dto/request/FavoriteReq.java
+++ src/main/java/baeksaitong/sofp/domain/favorite/dto/request/FavoriteReq.java
@@ -11,7 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class FavoriteReq {
     @Schema(description = "알약 고유 번호", example = "200808876")
     @NotNull(message = "알약 고유 번호가 필요합니다.")
-    private Long pillSeralNumber;
+    private Long pillSerialNumber;
 
     @Schema(description = "이미지 검색 시 사용한 이미지")
     private MultipartFile image;
