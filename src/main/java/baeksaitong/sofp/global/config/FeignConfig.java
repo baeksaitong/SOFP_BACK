07a14248c0517fb41839936e8ac1b47b71f55e4a
Jansoon <ckdals4600@naver.com>
2024-06-02 04:03:02 +0900

    :bug: Fix: XML 파싱에 따른 모든 응답 API XML 형식 변경 문제 해결 (#70)

--- src/main/java/baeksaitong/sofp/global/config/FeignConfig.java
+++ src/main/java/baeksaitong/sofp/global/config/FeignConfig.java
@@ -1,12 +1,6 @@
 package baeksaitong.sofp.global.config;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import feign.Client;
-import feign.codec.Decoder;
-import feign.codec.Encoder;
-import feign.jackson.JacksonDecoder;
-import feign.jackson.JacksonEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,22 +9,5 @@ public class FeignConfig {
     @Bean
     public Client feignClient() {
         return new Client.Default(null, null);
-    }
-
-    @Bean
-    public XmlMapper xmlMapper() {
-        XmlMapper xmlMapper = new XmlMapper();
-        xmlMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        return xmlMapper;
-    }
-
-    @Bean
-    public Encoder feignEncoder(XmlMapper xmlMapper) {
-        return new JacksonEncoder(xmlMapper);
-    }
-
-    @Bean
-    public Decoder feignDecoder(XmlMapper xmlMapper) {
-        return new JacksonDecoder(xmlMapper);
     }
 }
---
+++ src/main/java/baeksaitong/sofp/global/config/XmlFeignConfig.java
@@ -0,0 +1,36 @@
+package baeksaitong.sofp.global.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import feign.Client;
+import feign.codec.Decoder;
+import feign.codec.Encoder;
+import feign.jackson.JacksonDecoder;
+import feign.jackson.JacksonEncoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class XmlFeignConfig {
+    @Bean(name = "xmlFeignClient")
+    public Client xmlFeignClient() {
+        return new Client.Default(null, null);
+    }
+
+    @Bean
+    public XmlMapper xmlMapper() {
+        XmlMapper xmlMapper = new XmlMapper();
+        xmlMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return xmlMapper;
+    }
+
+    @Bean
+    public Encoder feignEncoder(XmlMapper xmlMapper) {
+        return new JacksonEncoder(xmlMapper);
+    }
+
+    @Bean
+    public Decoder feignDecoder(XmlMapper xmlMapper) {
+        return new JacksonDecoder(xmlMapper);
+    }
+}
