a4d7f30e3d0072bbd9a8be203b0d52c77f8c8fbc
Jansoon <ckdals4600@naver.com>
2024-06-04 11:25:12 +0900

    :sparkles: Feat: 스웨거 추가 (#70)

--- src/main/java/baeksaitong/sofp/domain/pharmacy/dto/response/OpeningHourDto.java
+++ src/main/java/baeksaitong/sofp/domain/pharmacy/dto/response/OpeningHourDto.java
@@ -1,9 +1,13 @@
 package baeksaitong.sofp.domain.pharmacy.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.LocalTime;
 
 public record OpeningHourDto (
-    LocalTime star,
-    LocalTime end
+        @Schema(description = "영업 시작 시간")
+        LocalTime start,
+        @Schema(description = "영업 마감 시간")
+        LocalTime end
 ){
 }
