b4b5418751ae903eca574f86663c7183ee844c1c
Jansoon <ckdals4600@naver.com>
2024-06-04 23:59:49 +0900

    :memo: Docs: 스웨거 추가 (#85)

--- src/main/java/baeksaitong/sofp/domain/category/dto/response/ProfileBasicDto.java
+++ src/main/java/baeksaitong/sofp/domain/category/dto/response/ProfileBasicDto.java
@@ -1,9 +1,12 @@
 package baeksaitong.sofp.domain.category.dto.response;
 
 import baeksaitong.sofp.domain.profile.entity.Profile;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public record ProfileBasicDto(
+        @Schema(description = "사용자 ID")
         String profileId,
+        @Schema(description = "색상")
         String color
 ) {
     public ProfileBasicDto(Profile profile){
